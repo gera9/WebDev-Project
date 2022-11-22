d9f713ec173e9b0fbbb78a7796f6ec9ee98d7dc7
Gerardo M. R <gerardo_9978@outlook.com>
2022-11-22 17:33:14 -0600

    fix: footer

--- server/pkg/mongo/scores.go
+++ server/pkg/mongo/scores.go
@@ -2,7 +2,10 @@ package mongo
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 
+	jsonpatch "github.com/evanphx/json-patch"
 	"github.com/gera9/WebDev-Project/server/pkg/models"
 	"go.mongodb.org/mongo-driver/bson"
 )
@@ -11,18 +14,28 @@ func (r *Repository) NewRecord(newRecord models.Score) (*models.Score, error) {
 	coll := r.client.Database(r.dbName).Collection("records")
 
 	filter := bson.M{"name": newRecord.Name}
-	update := bson.D{{Key: "$set", Value: newRecord}}
+	//update := bson.D{{Key: "$set", Value: newRecord}}
 
 	res := new(models.Score)
 
-	err := coll.FindOneAndUpdate(context.TODO(), filter, update).Decode(res)
+	ress, _ := json.Marshal(res)
+	newRecordd, _ := json.Marshal(newRecord)
+
+	_ = coll.FindOne(context.TODO(), filter).Decode(res)
+
+	patch, err := jsonpatch.MergePatch(ress, newRecordd)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("patch: %s\n", patch)
+	/* err := coll.FindOneAndUpdate(context.TODO(), filter, update).Decode(res)
 	if err != nil {
 		_, err := coll.InsertOne(context.TODO(), newRecord)
 		if err != nil {
 			return nil, err
 		}
 		return &newRecord, nil
-	}
+	} */
 
 	return res, nil
 }
